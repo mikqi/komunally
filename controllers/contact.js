35d6fae08d79f989d3c34b3c91b837d3d1192729
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-05-01 16:42:12 -0700

    edit some files

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
   service: 'SendGrid',
@@ -6,47 +6,47 @@ const transporter = nodemailer.createTransport({
     user: process.env.SENDGRID_USER,
     pass: process.env.SENDGRID_PASSWORD
   }
-});
+})
 
 /**
  * GET /contact
  * Contact form page.
  */
 exports.getContact = (req, res) => {
-  const unknownUser = !(req.user);
+  const unknownUser = !(req.user)
 
   res.render('contact', {
     title: 'Contact',
-    unknownUser,
-  });
-};
+    unknownUser
+  })
+}
 
 /**
  * POST /contact
  * Send a contact form via Nodemailer.
  */
 exports.postContact = (req, res) => {
-  let fromName;
-  let fromEmail;
+  let fromName
+  let fromEmail
   if (!req.user) {
-    req.assert('name', 'Name cannot be blank').notEmpty();
-    req.assert('email', 'Email is not valid').isEmail();
+    req.assert('name', 'Name cannot be blank').notEmpty()
+    req.assert('email', 'Email is not valid').isEmail()
   }
-  req.assert('message', 'Message cannot be blank').notEmpty();
+  req.assert('message', 'Message cannot be blank').notEmpty()
 
-  const errors = req.validationErrors();
+  const errors = req.validationErrors()
 
   if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/contact');
+    req.flash('errors', errors)
+    return res.redirect('/contact')
   }
 
   if (!req.user) {
-    fromName = req.body.name;
-    fromEmail = req.body.email;
+    fromName = req.body.name
+    fromEmail = req.body.email
   } else {
-    fromName = req.user.profile.name || '';
-    fromEmail = req.user.email;
+    fromName = req.user.profile.name || ''
+    fromEmail = req.user.email
   }
 
   const mailOptions = {
@@ -54,14 +54,14 @@ exports.postContact = (req, res) => {
     from: `${fromName} <${fromEmail}>`,
     subject: 'Contact Form | Hackathon Starter',
     text: req.body.message
-  };
+  }
 
   transporter.sendMail(mailOptions, (err) => {
     if (err) {
-      req.flash('errors', { msg: err.message });
-      return res.redirect('/contact');
+      req.flash('errors', { msg: err.message })
+      return res.redirect('/contact')
     }
-    req.flash('success', { msg: 'Email has been sent successfully!' });
-    res.redirect('/contact');
-  });
-};
+    req.flash('success', { msg: 'Email has been sent successfully!' })
+    res.redirect('/contact')
+  })
+}
